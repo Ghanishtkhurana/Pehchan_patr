472fd445c2a3064d227d26cf4f03a3bc5842b81b
ghanishtkhurana <ghanishtkhurana9@gmail.com>
2024-02-04 23:12:01 +0530

    font update

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -23,7 +23,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel='icon' href='/favicon.ico'/>
+        {/* <link rel='icon' href='/favicon.ico'/> */}
+        <link rel="icon" href="/eagle.jpeg" />
       </head>
       <body className={roboto.className}>
         <NxtProvider>{children}</NxtProvider>
